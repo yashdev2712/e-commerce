ac8fa1a647f5b6e029823030168975bf939dabb8
yashdev2712 <deveshchandra2712@gmail.com>
2024-07-13 02:53:08 +0530

    x

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const isLoading = false;
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center min-h-screen '>
+      <div className="w-full max-w-md bg-white rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 p-8 mx-4">
+        <h1 className='text-3xl font-semibold text-center mb-10'>Login</h1>
+        <span className='block text-center text-xl mb-8'>Enchanted Pages</span>
+
+
+        <div className='w-full mb-4'>
+          <label className='block text-lg mb-2'>Username</label>
+          <input type="text" placeholder="Enter your Username" className="input input-bordered input-primary w-full p-2" />
+        </div>
+
+        <div className='w-full mb-4'>
+          <label className='block text-lg mb-2'>Password</label>
+          <input type="password" placeholder="Enter your Password" className="input input-bordered input-primary w-full p-2" />
+        </div>
+
+        <Link to="/Signup" className='text-sm text-center hover:underline hover:text-blue-500 mt-2 inline-block'>
+          Don't have an account
+        </Link>
+
+        <div>
+          <button className="btn btn-block btn-sm mt-2"
+            disabled={isLoading}>
+            {isLoading ? <span className="loading loading-spinner"></span> : "SignUp"}
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 const Signup = () => {
+  const isLoading = false;
   return (
     <div className='flex items-center justify-center min-h-screen '>
       <div className="w-full max-w-md bg-white rounded-lg bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 p-8 mx-4">
@@ -28,9 +30,15 @@ const Signup = () => {
             <input type="password" placeholder="Enter your Password" className="input input-bordered input-primary w-full p-2" />
           </div>
         </div>
+
+        <Link to="/login" className='text-sm text-center hover:underline hover:text-blue-500 mt-2 inline-block'>
+          Already have an account
+        </Link>
+
         <div>
-          <button className="btn btn-square">
-            <span className="loading loading-spinner"></span>
+          <button className="btn btn-block btn-sm mt-2"
+            disabled={isLoading}>
+            {isLoading ? <span className="loading loading-spinner"></span> : "SignUp"}
           </button>
         </div>
       </div>
